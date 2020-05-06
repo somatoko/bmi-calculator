0b99053810d9603c3cc53e64bbfa1875bb6b644f
atctex <atctex@gmail.com>
2020-05-06 15:21:53 +0200

    Updates

---
+++ precache-manifest.c8f6a881163c06f7187669c20f36a028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fbe4220ba45113611eaf230f32b308",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "867d3ecbc5526d4a1822",
+    "url": "/bmi-calculator/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2accb06b0dc5ebcb2632",
+    "url": "/bmi-calculator/static/css/main.cd8778d8.chunk.css"
+  },
+  {
+    "revision": "867d3ecbc5526d4a1822",
+    "url": "/bmi-calculator/static/js/2.f7442644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bmi-calculator/static/js/2.f7442644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2accb06b0dc5ebcb2632",
+    "url": "/bmi-calculator/static/js/main.b3529d24.chunk.js"
+  },
+  {
+    "revision": "b0d9f7a354bbb539ef1e",
+    "url": "/bmi-calculator/static/js/runtime-main.7cfc36d9.js"
+  }
+]);
